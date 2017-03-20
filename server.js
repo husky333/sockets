28e2507c9cbc12d998c7f10b14013c3bdc1dfe53
Chris Hardin <chrishusky27@gmail.com>
2017-03-20 17:18:55 -0600

    browsers can talk to each other via message event

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
-var socket = require('socket.io')(http);
+var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-socket.on('connection', function() {
+io.on('connection', function(socket) {
   console.log('connected with socket io');
+
+  socket.on('message', function(message) {
+    console.log('Message received: ' + message.text);
+
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.emit('message', {
+    text: 'Welcome to the chat application'
+  });
 });
 
 http.listen(PORT, function() {
