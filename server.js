863d9d77b6d11f8b8867c933fe2e8afe5839bf4f
Chris Hardin <chrishusky27@gmail.com>
2017-03-20 16:57:09 -0600

    added front end connection to socket io

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ var PORT = process.env.PORT || 3000;
 var express = require('express');
 var app = express();
 var http = require('http').Server(app);
+var socket = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
+socket.on('connection', function() {
+  console.log('connected with socket io');
+});
+
 http.listen(PORT, function() {
   console.log('Server started');
-})
+});
